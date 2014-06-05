fb3e382c9dedb72fb216b330e239f19d4cb734b2
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-05 17:53:25 -0400

    sets up initial callstack & designates public functions for testing

--- js/game.js
+++ js/game.js
@@ -6,15 +6,60 @@ var GAME = (function() {
 
   // private vars
   var _currentState,
-      _nextState;
+      _nextState,
+      _step_timeout = 750;
 
-  api.init = function(state) {
-    _currentState = state || makeInitialState();
+  /**
+   * Inits the app or takes a provided state object and iterates to the next step.
+   * Called recursively unless second parameter passed as true.
+   * 
+   * @public
+   */
+  api.nextStep = function(state, dontRecurse) {
+    _currentState = state || api.makeInitialState();
 
     console.log(_currentState);
+    
+    api.drawTableBasedOnState(_currentState);
+    _nextState = api.prepareNextStep(_currentState);
+
+    if (!dontRecurse) {
+      setTimeout(function() {
+        api.init(_nextState);
+      }, _step_timeout);
+    }
   }
 
-  function makeInitialState() {
+  /**
+   * Draws a table into the DOM based on the provided 
+   * state array
+   * 
+   * @param  {Array} state Double sided array of objects containing an "on" key which is boolean
+   * @public
+   */
+  api.drawTableBasedOnState = function(state) {
+
+  }
+
+  /**
+   * Prepares a brand new double sided array to be the state used for
+   * the next step
+   *
+   * @param {Array} state Previous state object
+   * @returns {Array} Double sided array of objects; the state for the next step
+   */
+  api.prepareNextStep = function(state) {
+
+  }
+
+  /**
+   * Makes the initial double sided array of objects
+   * representing each column
+   * Expects that the return array[3][5].on == true
+   *
+   * @public
+   */
+  api.makeInitialState = function() {
     var initialState = [];
 
     for (var i = 0; i < 10; i++) {
@@ -34,4 +79,4 @@ var GAME = (function() {
   return api;
 }())
 
-GAME.init();
+GAME.nextStep();
